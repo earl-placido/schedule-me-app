2d8b2c2c4a6f8c5a4bcb7a6f261cdb4c87b0d1e3
Earl <placidej@myumanitoba.ca>
2020-02-04 13:11:27 -0600

    forgot to rename

--- server/api/routes/exampleRouter.js
+++ server/api/routes/exampleRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 const responses = require('../util/responses');
 const router = express.Router();
 
-const exampleController = require('../model/exampleModel');
+const exampleModel = require('../model/exampleModel');
 
 router.get('/:id', (req, res, next) => {
     const { id } = req.params;
     
-    return exampleController.getData(id)
+    return exampleModel.getData(id)
         .then(data => res.status(responses.SUCCESS).json(data))
         .catch(next);
 });
@@ -16,7 +16,7 @@ router.post('/:id', (req, res, next) => {
     const { id } = req.params;
     const data = req.body.data;
     
-    return exampleController.insertData(id, data)
+    return exampleModel.insertData(id, data)
         .then(result => res.status(responses.CREATED).json(result[0]))
         .catch(next);
 });
