c51780bb5f8188bab60635ef4a0c410e6c546a33
Syndrical <hoangk@myumanitoba.ca>
2020-02-22 20:22:12 -0600

    adjust file location for tests

---
+++ client/web/src/tests/components/groups/GroupMeetingForm/GroupMeetingForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import GroupMeetingForm from '../../../../components/groups/GroupMeetingForm/GroupMeetingForm';
+
+test('Renders', () => {
+  render(<GroupMeetingForm />);
+});
