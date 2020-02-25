8889eccabfed015954b9a4f35d7e685c7ef835af
Daryl Fung <darylfung96@gmail.com>
2020-02-24 18:35:06 -0600

    fix test cases

--- client/web/src/tests/components/groups/GroupInfoForm/GroupInfoForm.test.js
+++ client/web/src/tests/components/groups/GroupInfoForm/GroupInfoForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import GroupInfoForm from '../../../components/groups/GroupInfoForm';
+import GroupInfoForm from '../../../../components/groups/GroupInfoForm';
 
 
 
