a0d51e887004b4e912e2ca341c2dae3966ac8771
Daryl Fung <darylfung96@gmail.com>
2020-03-04 12:08:18 -0600

    added logic to repeat events weekly

--- client/web/src/actions/components/screens/Group.action.js
+++ client/web/src/actions/components/screens/Group.action.js
@@ -1,8 +1,11 @@
+import moment from 'moment';
+
 export const SELECT_DATE = 'select_date';
 export const SHOW_MODAL = 'show_modal';
 export const HANDLE_OK = 'handle_ok';
 export const CANCEL_AVAILABILITY = 'cancel_availability';
 export const DELETE_AVAILABILITY = 'delete_availability';
+export const ADD_AVAILABILITY = 'add_availability';
 export const ADD_RANGE = 'add_range';
 export const CHANGE_RANGE = 'change_range';
 
@@ -14,7 +17,7 @@ export const CHANGE_RANGE = 'change_range';
 //     4: "Thursday",
 //     5: "Friday",
 //     6: "Saturday",
-//     7: "Sunday"
+//     0: "Sunday"
 // };
 
 export const selectDate = (date) => {
@@ -45,7 +48,17 @@ export const deleteAvailability = (rangeHours) => {
 
     return {
         type: DELETE_AVAILABILITY,
-        payload: rangeHours
+        payload: newRangeHours
+    };
+};
+
+export const addAvailability = (selectedDate, rangeHours, availableDays) => {
+    //currently doesn't check for clashing of time
+    const day = selectedDate.day();
+    availableDays[day] = rangeHours;
+    return {
+        type: ADD_AVAILABILITY, 
+        payload: {availableDays, modalVisible: false, rangeHours: ['']}
     };
 };
 
@@ -65,11 +78,34 @@ export const onChangeRange = (index, value, rangeHours) => {
     };
 };
 
-// export const onMonthChange = value => {
-//     // check if month changed
-// };
+const convertDatesToDay = (currentYear, currentMonth) => {
+    const firstDateOfMonth = new Date(currentYear, currentMonth, 1);
+    const lastDateOfMonth = new Date(currentYear, currentMonth+1, 0);
 
-const INITIAL_STATE = {modalVisible: false, rangeHours: [''], selectedDate: ''};
+    let datesToDay = {}
+
+    // calculate all the dates for a given day (index refers to day: monday, tuesday, ...)
+    // {Monday: [1,8,15,22,29], Tuesday: [2,9,16,23,30], ...}
+    for (let dayIndex = 1; dayIndex < 8; dayIndex++) {
+        firstDateOfMonth.getDay();
+
+        let currentDate = dayIndex;
+        datesToDay[currentDate] = (firstDateOfMonth.getDay() + (dayIndex-1)) % 6; // convert date to day
+        currentDate += 7;
+        while (currentDate <= lastDateOfMonth) 
+            datesToDay[currentDate] = (firstDateOfMonth.getDay() + (dayIndex-1)) % 6;
+    }
+
+    return datesToDay;
+
+};
+
+export const onPanelChange = (value, availableDays) => {
+    // check if month changed
+
+};
+
+const INITIAL_STATE = {modalVisible: false, rangeHours: [''], selectedDate: '', availableDays: {}};
 
 export default(state=INITIAL_STATE, action) => {
     switch(action.type) {
@@ -85,11 +121,16 @@ export default(state=INITIAL_STATE, action) => {
         case(DELETE_AVAILABILITY): {
             return {...state, rangeHours: action.payload};
         }
+        case (ADD_AVAILABILITY): {
+            return {...state, ...action.payload};
+        }
         case (ADD_RANGE): {
             return {...state, rangeHours: action.payload};
         }
         case(CHANGE_RANGE): {
             return {...state, rangeHours: action.payload};
         }
+        default:
+            return INITIAL_STATE;
     }
 };
--- client/web/src/components/screens/Group/Group.js
+++ client/web/src/components/screens/Group/Group.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react';
-import {Calendar, Modal, TimePicker, Button, Checkbox} from 'antd';
+import {Calendar, Modal, TimePicker, Button, Checkbox, Badge} from 'antd';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
-import {selectDate, showModal, cancelAvailability, deleteAvailability,
-     handleAdd, onChangeRange, onMonthChange} from '../../../actions/components/screens/Group.action';
+import {selectDate, showModal, cancelAvailability, deleteAvailability, addAvailability,
+     handleAdd, onChangeRange} from '../../../actions/components/screens/Group.action';
 
 const {RangePicker} = TimePicker;
 
 class Group extends Component {
 
-    state = {modalVisible: false, rangeHours: [''], selectedDate: ''};
 
     onSelect = (value) => {
         this.props.selectDate(value);
@@ -21,7 +20,7 @@ class Group extends Component {
     }
 
     handleOk = () => {
-        // todo add avaiability
+        this.props.addAvailability(this.props.selectedDate, this.props.rangeHours, this.props.availableDays);
     }
 
     handleCancel = () => {
@@ -40,23 +39,38 @@ class Group extends Component {
         this.props.onChangeRange(index, value, this.props.rangeHours);
     }
 
-    // onPanelChange = value => {
-    //     // check if month changed
-    // };
+    dateCellRender = (value) => {
+        const availability = this.props.availableDays[value.day()];
+        if (availability === undefined) return;
+
+        return (
+            <ul className="events">
+              {availability.map((item, index) => (
+                <li key={index}>
+                  <Badge status={'success'} text={item[0].format('HH:mm') + '-' + item[1].format('HH:mm')} />
+                </li>
+              ))}
+            </ul>
+          );
+    };
+
+    onPanelChange = value => {
+        // check if month changed
+    };
 
     render() {
         return (
             <div>
                 <Button onClick={this.showModal} type="primary" >Add Availability</Button>
-                <Calendar onSelect={this.onSelect} onPanelChange={this.onPanelChange}/>
+                <Calendar onSelect={this.onSelect} mode='month' dateCellRender={this.dateCellRender} onPanelChange={this.onPanelChange}/>
 
                 <Modal visible={this.props.modalVisible} onOk={this.handleOk} onCancel={this.handleCancel}>
-                    <h2>{this.props.selectedDate}</h2>
+                    <h2>{this.props.selectedDate && this.props.selectedDate.format('YYYY-MMM-DD (dddd)')}</h2>
                     <h3 className='modal-header'>Input availability time</h3>
                     {this.props.rangeHours.map( (item, index) => {
                         return (
                         <div key={index} className='range-picker'>
-                            <RangePicker onChange={this.onChangeRange.bind(this, index)}/>
+                            <RangePicker value={this.props.rangeHours[index]} onChange={this.onChangeRange.bind(this, index)}/>
                         </div>
                         );
                     })}
@@ -75,8 +89,8 @@ class Group extends Component {
 }
 
 const mapStateToProps = ({ AddAvailabilityReducer }) => {
-    const {modalVisible, rangeHours, selectedDate} = AddAvailabilityReducer;
-    return {modalVisible, rangeHours, selectedDate};
+    const {modalVisible, rangeHours, selectedDate, availableDays} = AddAvailabilityReducer;
+    return {modalVisible, rangeHours, selectedDate, availableDays};
 };
 
 Group.propTypes = {
@@ -87,6 +101,7 @@ Group.propTypes = {
     rangeHours: PropTypes.any,
     modalVisible: PropTypes.any,
     selectedDate: PropTypes.any,
+    availableDays: PropTypes.any,
     
     
     handleAdd: PropTypes.func,
@@ -95,5 +110,5 @@ Group.propTypes = {
 };
 
 export default connect(mapStateToProps, 
-    {selectDate, showModal, cancelAvailability, deleteAvailability,
-        handleAdd, onChangeRange, onMonthChange})(Group);
+    {selectDate, showModal, cancelAvailability, deleteAvailability, addAvailability,
+        handleAdd, onChangeRange})(Group);
