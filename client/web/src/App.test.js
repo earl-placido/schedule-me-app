f10da1a835b95dc83139de9576b7ca0407b07e0b
Daryl Fung <darylfung96@gmail.com>
2020-03-06 02:06:38 -0600

    fix test error in web

--- client/web/src/App.test.js
+++ client/web/src/App.test.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { mount, configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import App from './App';
 
 configure({adapter: new Adapter()});
 
-test('Renders', () => {
-  const mockStore = configureStore();
-  const store = mockStore({
-    auth: {},
+describe('test app', () => {
+
+  beforeAll(() => {  
+    Object.defineProperty(window, "matchMedia", {
+      value: jest.fn(() => { 
+        return { 
+          matches: true,
+          addEventListener: jest.fn(),
+          removeEventListener: jest.fn()
+        } 
+      })
+    });
   });
-  const wrapper = mount(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-  wrapper.debug();
-  expect(wrapper.find('.app')).toHaveLength(1);
+
+  it('Renders', () => {
+    const mockStore = configureStore();
+    const store = mockStore({
+      auth: {},
+    });
+    const wrapper = shallow(
+        <App store={store }/>
+    ).dive();
+    console.log(wrapper.debug());
+    expect(wrapper.dive().find('.app')).toHaveLength(1);
+  });
 });
+
