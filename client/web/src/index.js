5c8e3f21fd95d5c05f68ffb8ced1b1c2cf1e38ee
Brenna <brennanatashaepp@gmail.com>
2020-03-06 00:46:43 -0600

    Added lint script

--- client/web/src/index.js
+++ client/web/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import Reducers from './Reducers';
+import thunk from 'redux-thunk'
+import Reducers from './Reducers'
 
 import './index.css';
 import App from './App';
