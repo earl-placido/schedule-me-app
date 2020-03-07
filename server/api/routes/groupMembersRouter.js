dadb13120e7d70aae69056bf23c981dcdc824136
Renz <cabusasr@myumanitoba.ca>
2020-03-07 14:53:53 -0600

    Fix linting error by running 'yarn run lint --fix'

--- server/api/routes/groupMembersRouter.js
+++ server/api/routes/groupMembersRouter.js
@@ -5,17 +5,19 @@ const groupsModel = require("../model/groupsModel");
 
 // Get all group members in a group
 router.get("/:groupId", (req, res, next) => {
-    const groupId = req.params.groupId; 
-    
-    if (!groupId) {
-        res.status(responses.NOT_FOUND);
-        res.send({ error: `groupId is required.` });
-    } else {
-        return groupsModel
-        .getGroupMembers(groupId)
-        .then(result => res.status(responses.SUCCESS).json({ groupMembers: result }))
-        .catch(next);
-    }
+  const groupId = req.params.groupId;
+
+  if (!groupId) {
+    res.status(responses.NOT_FOUND);
+    res.send({ error: `groupId is required.` });
+  } else {
+    return groupsModel
+      .getGroupMembers(groupId)
+      .then(result =>
+        res.status(responses.SUCCESS).json({ groupMembers: result })
+      )
+      .catch(next);
+  }
 });
 
 module.exports = router;
