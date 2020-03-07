f417ec38e8a66a8b4dc29cc97bfd99ed510e2ba2
Brenna <brennanatashaepp@gmail.com>
2020-03-06 23:59:37 -0600

    Send auth token with request for grouplist

--- client/web/src/Reducers.js
+++ client/web/src/Reducers.js
@@ -3,7 +3,7 @@ import { combineReducers } from "redux";
 import CreateGroupReducer from "./actions/components/screens/CreateGroup.action";
 import LoginModalReducer from "./actions/components/login/LoginModal.action";
 import AuthReducer from "./actions/components/screens/Auth.action";
-import GroupListReducer from './actions/components/screens/GroupList.action';
+import GroupListReducer from "./actions/components/screens/GroupList.action";
 
 export default combineReducers({
   CreateGroupReducer,
