3ed8ed74ac8305e11e9d852385fcc8737bc7a902
Kevin Hoang <hoangk@myumanitoba.ca>
2020-03-06 19:56:31 -0600

    current progress

--- client/web/src/components/screens/GroupList/GroupList.js
+++ client/web/src/components/screens/GroupList/GroupList.js
@@ -1,37 +1,46 @@
 import React, { Component } from "react";
-import { Row, Card, List, Icon, Avatar, Divider, Typography } from "antd";
-import "antd/dist/antd.css";
 
-export default class GroupList extends Component {
+import { Row, Card, List, Icon, Avatar, Divider, Typography } from "antd";
+import { getGroupList } from '../../../actions/components/screens/GroupList.action';
+import "antd/dist/antd.css";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class GroupList extends Component {
   render() {
+    let groupList;
+    this.props.getGroupList().then(function(result) {
+      groupList = result;
+    });
+
     const { Title } = Typography;
     const { containerStyle, cardStyle, dividerStyle, titleStyle } = styles;
-    const groupList = [
-      {
-        groupId: "1",
-        groupName: "Equilibrium",
-        groupDescription: "For everyone, everything, equally",
-        groupOwnerId: "277"
-      },
-      {
-        groupId: "2",
-        groupName: "Schedule McScheduleFace",
-        groupDescription: "A rip off of Boaty McBoatFace",
-        groupOwnerId: "278"
-      },
-      {
-        groupId: "3",
-        groupName: "Anti-Hate Club",
-        groupDescription: "All things about hating hate",
-        groupOwnerId: "279"
-      },
-      {
-        groupId: "4",
-        groupName: "Im With Stupid ^",
-        groupDescription: "Im With Stupid ^",
-        groupOwnerId: "210"
-      }
-    ];
+    // const groupList = [
+    //   {
+    //     groupId: "1",
+    //     groupName: "Equilibrium",
+    //     groupDescription: "For everyone, everything, equally",
+    //     groupOwnerId: "277"
+    //   },
+    //   {
+    //     groupId: "2",
+    //     groupName: "Schedule McScheduleFace",
+    //     groupDescription: "A rip off of Boaty McBoatFace",
+    //     groupOwnerId: "278"
+    //   },
+    //   {
+    //     groupId: "3",
+    //     groupName: "Anti-Hate Club",
+    //     groupDescription: "All things about hating hate",
+    //     groupOwnerId: "279"
+    //   },
+    //   {
+    //     groupId: "4",
+    //     groupName: "Im With Stupid ^",
+    //     groupDescription: "Im With Stupid ^",
+    //     groupOwnerId: "210"
+    //   }
+    // ];
     return (
       <div style={containerStyle}>
         <Card style={cardStyle}>
@@ -81,3 +90,15 @@ const styles = {
     textAlign: "center"
   }
 };
+
+const mapStateToProps = ({ GroupListReducer }) => {
+  const {groupListInformation} = GroupListReducer;
+  return {groupListInformation};
+};
+
+GroupList.propTypes = {
+  getGroupList: PropTypes.any,
+};
+
+export default connect(mapStateToProps, 
+  {getGroupList})(GroupList); 
