40d3cd2f0f0cf4e09189715164e2612b717ba19b
Brenna <brennanatashaepp@gmail.com>
2020-03-08 01:44:15 -0600

    Remove comment

--- client/web/src/components/screens/CreateGroup/CreateGroup.js
+++ client/web/src/components/screens/CreateGroup/CreateGroup.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Steps, Row, Col, Card, Button, Icon } from "antd";
 import { connect } from "react-redux";
-import PropTypes from "prop-types"; // to prevent eslint from showing errors that props not found
+import PropTypes from "prop-types";
 
 import GroupInfoForm from "../../groups/GroupInfoForm";
 import GroupMeetingForm from "../../groups/GroupMeetingForm";
