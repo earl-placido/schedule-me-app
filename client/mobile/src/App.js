6b7de00876c3bddb225c30d4e68d81a2df463d97
Jennifer Seo <jenniferCSeo@gmail.com>
2020-03-08 19:21:53 -0500

    Add functionality for switching between pages

--- client/mobile/src/App.js
+++ client/mobile/src/App.js
@@ -1,32 +1,80 @@
-import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
-import {Container, Header, Content, Title} from 'native-base';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Container, Footer, Content, Title} from 'native-base';
+import {Button, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
 
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import Reducers from './Reducers';
+
 import GroupDetail from './components/screen/GroupDetail';
+import CreateGroup from './components/screen/CreateGroup';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Provider store={createStore(Reducers, {}, applyMiddleware(thunk))}>
-        <Container>
-          <Header style={styles.headerStyle}>
-            <Title>schedule-me-up</Title>
-          </Header>
-          <GroupDetail />
-
-          <Content />
-        </Container>
-      </Provider>
-    );
-  }
+function HomeScreen({navigation}) {
+  return (
+    <Container>
+      <Button
+        title="Go to group details"
+        onPress={() => navigation.navigate('Group Detail')}
+      />
+      <CreateGroup />
+      <Content />
+    </Container>
+  );
 }
+
+function DetailsScreen({navigation}) {
+  return (
+    <Container>
+      <Button
+        title="Go to create group"
+        onPress={() => navigation.navigate('Create Group')}
+      />
+      <GroupDetail />
+      <Content />
+    </Container>
+  );
+}
+
+DetailsScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <Provider store={createStore(Reducers, {}, applyMiddleware(thunk))}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Create Group" component={HomeScreen} />
+          <Stack.Screen name="Group Detail" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      <Footer style={styles.headerStyle}>
+        <Title>schedule-me-up</Title>
+      </Footer>
+    </Provider>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   headerStyle: {
-    padding: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
