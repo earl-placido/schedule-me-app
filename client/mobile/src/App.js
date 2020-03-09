c00eec1c0bbb92dfbff53e4412722e8831bfe3ae
Jennifer Seo <jenniferCSeo@gmail.com>
2020-03-09 14:04:22 -0500

    Fix button placement on GroupDetail page

--- client/mobile/src/App.js
+++ client/mobile/src/App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Container, Footer, Content, Title} from 'native-base';
@@ -13,7 +13,7 @@ import Reducers from './Reducers';
 import GroupDetail from './components/screen/GroupDetail';
 import CreateGroup from './components/screen/CreateGroup';
 
-function HomeScreen({navigation}) {
+function DetailsScreen({navigation}) {
   return (
     <Container>
       <Button
@@ -26,7 +26,7 @@ function HomeScreen({navigation}) {
   );
 }
 
-function DetailsScreen({navigation}) {
+function HomeScreen({navigation}) {
   return (
     <Container>
       <Button
@@ -39,6 +39,34 @@ function DetailsScreen({navigation}) {
   );
 }
 
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(Reducers, {}, applyMiddleware(thunk))}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Group Detail" component={HomeScreen} />
+            <Stack.Screen name="Create Group" component={DetailsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+
+        <Footer style={styles.footerStyle}>
+          <Title>schedule-me-up</Title>
+        </Footer>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  footerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 DetailsScreen.propTypes = {
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
@@ -50,31 +78,3 @@ HomeScreen.propTypes = {
     navigate: PropTypes.func.isRequired,
   }).isRequired,
 };
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <Provider store={createStore(Reducers, {}, applyMiddleware(thunk))}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Create Group" component={HomeScreen} />
-          <Stack.Screen name="Group Detail" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-
-      <Footer style={styles.headerStyle}>
-        <Title>schedule-me-up</Title>
-      </Footer>
-    </Provider>
-  );
-}
-
-export default App;
-
-const styles = StyleSheet.create({
-  headerStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
--- client/mobile/src/components/screen/GroupDetail.js
+++ client/mobile/src/components/screen/GroupDetail.js
@@ -1,93 +1,109 @@
 import React, {Component} from 'react';
-import {Left, Body, Button, CardItem, Card} from 'native-base';
-import {Image, FlatList, StyleSheet, Text, View} from 'react-native';
+import {Alert, Image, View, FlatList, StyleSheet, Text} from 'react-native';
+import {Body, Container, Content, Card, CardItem, Button} from 'native-base';
+
+import PropTypes from 'prop-types';
 
 const userList = [
   {
     name: 'Renz Cabusas',
     role: 'Administrator',
+    profilePic: 'https://reactjs.org/logo-og.png',
   },
   {
     name: 'Brenna Epp',
     role: 'Member',
+    profilePic: 'https://reactjs.org/logo-og.png',
   },
   {
     name: 'Bonnie Tang',
     role: 'Member',
+    profilePic: 'https://reactjs.org/logo-og.png',
   },
   {
     name: 'Daryl Fung',
     role: 'Member',
+    profilePic: 'https://reactjs.org/logo-og.png',
   },
   {
     name: 'Winnie the Pooh',
     role: 'Honey Eater',
+    profilePic: 'https://reactjs.org/logo-og.png',
   },
   {
     name: 'Piglet',
     role: 'Moral Support',
+    profilePic: 'https://reactjs.org/logo-og.png',
   },
 ];
 
-export default class GroupDetail extends Component {
-  keyExtractor = (item, index) => index.tostring + item.name;
+function DisplayUserDescription({title}) {
+  return (
+    <View>
+      <Text style={{marginLeft: 10}}>{title}</Text>
+    </View>
+  );
+}
 
+export default class GroupDetail extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <Card>
-          <CardItem header bordered>
-            <Body>
-              <Text style={({fontWeight: 'bold'}, {fontSize: 20})}>
-                Group Equilibrium
-              </Text>
-              <Text style={{fontSize: 15}}>
-                Optimal Time: 12pm - 2pm on October 26, 1985
-              </Text>
-            </Body>
-          </CardItem>
+      <Container style={styles.containerStyle}>
+        <Content>
+          <Card style={{flexDirection: 'column'}}>
+            <CardItem header bordered>
+              <Body>
+                <Text style={({fontWeight: 'bold'}, {fontSize: 20})}>
+                  Group Equilibrium
+                </Text>
+                <Text style={{fontSize: 15}}>
+                  Optimal Time: 12pm - 2pm on October 26, 1985
+                </Text>
+              </Body>
+            </CardItem>
 
-          <FlatList
-            showsHorizontalScrollIndicator={true}
-            data={userList}
-            keyExtractor={this.keyExtractor}
-            renderItem={({item}) => {
-              {
-                return (
-                  <View>
-                    <CardItem bordered>
-                      <Left>
-                        <Image
-                          style={{resizeMode: 'cover', width: 50, height: 50}}
-                          source={{uri: 'https://reactjs.org/logo-og.png'}}
-                        />
-                        <Body>
-                          <Text>{item.name}</Text>
-                          <Text>{item.role}</Text>
-                        </Body>
-                      </Left>
-                    </CardItem>
-                  </View>
-                );
-              }
-            }}
-          />
-        </Card>
+            <FlatList
+              showsHorizontalScrollIndicator={true}
+              data={userList}
+              renderItem={({item}) => (
+                <CardItem boardered>
+                  <Image
+                    style={{resizeMode: 'cover', width: 50, height: 50}}
+                    resizeMode={'cover'}
+                    source={{uri: item.profilePic}}
+                  />
+                  <Body>
+                    <DisplayUserDescription title={item.name} />
+                    <DisplayUserDescription title={item.role} />
+                  </Body>
+                </CardItem>
+              )}
+              keyExtractor={item => item.id}
+            />
+          </Card>
+        </Content>
 
         <View style={styles.viewCenter}>
-          <Button primary style={styles.buttonStyle}>
-            <Text style={{color: 'white'}}> Input Your Availability </Text>
+          <Button
+            style={styles.buttonStyle}
+            onPress={() => Alert.alert('Functionality to come')}>
+            <Text style={{color: 'white'}}>Input Your Availability</Text>
           </Button>
         </View>
-      </View>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 5,
+  },
   buttonStyle: {
     marginTop: 20,
     padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   viewCenter: {
@@ -96,3 +112,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
+
+DisplayUserDescription.propTypes = {
+  title: PropTypes.string.isRequired,
+};
