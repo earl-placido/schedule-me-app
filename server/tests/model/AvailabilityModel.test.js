c39522fbffc43a24208ece9a1c8653eb879b0a92
Daryl Fung <darylfung96@gmail.com>
2020-03-09 22:44:01 -0500

    fix eslint on server

--- server/tests/model/AvailabilityModel.test.js
+++ server/tests/model/AvailabilityModel.test.js
@@ -44,10 +44,14 @@ describe("test availability model", () => {
     const groupMemberId = 1;
     const data = await availabilityModel.getAvailability(groupMemberId);
     expect(data[0].AvailabilityId).toEqual(1);
-    expect(data[0].StartTime.toString()).toEqual("Mon Mar 09 2020 16:53:14 GMT-0500 (Central Daylight Time)");
+    expect(data[0].StartTime.toString()).toEqual(
+      "Mon Mar 09 2020 16:53:14 GMT-0500 (Central Daylight Time)"
+    );
 
     expect(data[1].AvailabilityId).toEqual(2);
-    expect(data[1].EndTime.toString()).toEqual("Mon Mar 09 2020 16:53:14 GMT-0500 (Central Daylight Time)");
+    expect(data[1].EndTime.toString()).toEqual(
+      "Mon Mar 09 2020 16:53:14 GMT-0500 (Central Daylight Time)"
+    );
   });
 
   it("add availabilities", async () => {
