c34b08ea6f6aa75c85a3bfdd935216de5af0f01d
Kevin Hoang <hoangk@myumanitoba.ca>
2020-03-16 00:59:29 -0500

    updated share link page to include code, button, and styling edits

--- client/web/src/components/groups/ShareLinkForm.js
+++ client/web/src/components/groups/ShareLinkForm.js
@@ -1,27 +1,64 @@
 import React, { Component } from "react";
-import { Input, Button, Row } from "antd";
+import { Button, Row, Typography, Col, message } from "antd";
+import { CopyOutlined } from "@ant-design/icons";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import PropTypes from "prop-types";
 
 class ShareLinkForm extends Component {
+  retrieveCodeFromLink(link) {
+    var theLink = link.split("/");
+    return theLink[theLink.length - 2];
+  }
+
+  success() {
+    message.success("Code copied!");
+  }
+
   render() {
+    const { Title } = Typography;
+    const { buttonStyle } = styles;
     return (
       <div className="center-container">
         <Row justify="center">
           <h3>
-            Your group has been created! The link for the group can be found
+            Your group has been created! The code for the group can be found
             here:
           </h3>
-          <Input value={this.props.link} />
         </Row>
         <Row justify="center">
+          <Col>
+            <Title level={3}>
+              {this.retrieveCodeFromLink(this.props.link)}
+            </Title>
+          </Col>
+          <Col offset={1}>
+            <CopyToClipboard
+              onCopy={() => this.success()}
+              text={this.retrieveCodeFromLink(this.props.link)}
+            >
+              <Button icon={<CopyOutlined />} />
+            </CopyToClipboard>
+          </Col>
+        </Row>
+        <Row justify="center" style={buttonStyle}>
           <Button type="primary" href={this.props.link}>
-            Go to Group Page
+            Go to Group
           </Button>
         </Row>
       </div>
     );
   }
 }
+
+const styles = {
+  buttonStyle: {
+    margin: 30
+  },
+
+  inputStyle: {
+    width: 200
+  }
+};
 
 ShareLinkForm.propTypes = {
   link: PropTypes.any
