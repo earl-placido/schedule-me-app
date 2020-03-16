0ac93f7eb90635bfe4caf61419a42a268749c95e
Ella <e.osinaike@yahoo.com>
2020-03-16 15:05:19 -0500

    Added list of hours to be selected

--- client/mobile/src/components/screen/InputAvailability.js
+++ client/mobile/src/components/screen/InputAvailability.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
-import {View, Button, Text} from 'native-base';
+import { StyleSheet, FlatList, View, TouchableWithoutFeedback} from 'react-native';
+import {Button, Text} from 'native-base';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 //import { addLocale } from 'react-native-week-view';
 
@@ -11,17 +11,42 @@ export default class InputAvailability extends Component {
     render() {
         return (
             <View>
-                <Button oogleButtonStyle>
+                <Button 
+                 googleButtonStyle 
+                 >
                     <Text>Input Availability</Text>
                 </Button>
-                 <Calendar
+                  <Calendar
                     onPressArrowLeft={substractMonth => substractMonth()}
                     // Handler which gets executed when press arrow icon right. It receive a callback can go next month
                     onPressArrowRight={addMonth => addMonth()}
                     // Handler which gets executed on day press. Default = undefined
                     onDayPress={(day) => {console.log('selected day', day)}}
                     >
-                </Calendar> 
+                  </Calendar>
+                 
+                  <FlatList  
+                    data = {[
+                      {key: '12:00am'},
+                      {key: '1:00am'},
+                      {key: '2:00am'},
+                      {key: '3:00am'},
+                      {key: '4:00am'},
+                      {key: '5:00am'},
+                      {key: '6:00am'},
+                      {key: '7:00am'},
+                      {key: '8:00am'},
+                      {key: '9:00am'},
+                      {key: '10:00am'},
+                      {key: '11:00am'},
+                      {key: '12:00pm'},
+                    ]}
+                    renderItem={({item}) => <Button border><Text >{item.key}</Text></Button>
+                     }
+                    
+                  />
+
+                
                 
                 
             </View>
@@ -43,5 +68,19 @@ const styles = StyleSheet.create({
   googleButtonStyle: {
     alignItems: 'center',
     marginBottom: 10,
+    fontSize: 50,
   },
+  itemList: {
+    fontSize: 30,
+    borderWidth: 1,
+    borderColor: 'blue',
+    //color: 'blue',
+    borderRadius: 1,
+    borderStyle: 'solid',
+    //padding: '1px',
+
+  },
+  title:{
+    fontSize: 50,
+  }
 });
