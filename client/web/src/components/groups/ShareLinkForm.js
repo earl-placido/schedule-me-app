f76cd1ce3f39c3b1ebaff12b3fa5bf5338b845fc
Kevin Hoang <hoangk@myumanitoba.ca>
2020-03-15 23:28:45 -0500

    added gotogroup button after creating group

--- client/web/src/components/groups/ShareLinkForm.js
+++ client/web/src/components/groups/ShareLinkForm.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-import { Input } from "antd";
+import { Input, Button, Row } from "antd";
 import PropTypes from "prop-types";
 
 class ShareLinkForm extends Component {
   render() {
     return (
       <div className="center-container">
-        <h3>
-          Your group has been created! The link for the group can be found here:
-        </h3>
-        <Input value={this.props.link} />
+        <Row justify="center">
+          <h3>
+            Your group has been created! The link for the group can be found
+            here:
+          </h3>
+          <Input value={this.props.link} />
+        </Row>
+        <Row justify="center">
+          <Button type="primary" href={this.props.link}>
+            Go to Group Page
+          </Button>
+        </Row>
       </div>
     );
   }
