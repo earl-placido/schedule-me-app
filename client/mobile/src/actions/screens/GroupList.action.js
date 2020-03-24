5efc1769d7ece3d9dfee755aaa7bf7de99e99ed1
Renz <cabusasr@myumanitoba.ca>
2020-03-24 18:55:12 -0500

    Fix server call

--- client/mobile/src/actions/screens/GroupList.action.js
+++ client/mobile/src/actions/screens/GroupList.action.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Config from 'react-native-config';
 import responses from '../util/responses';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export const GET_GROUP_LIST_SUCCESS = 'get_group_list_success';
 export const GET_GROUP_LIST_FAILURE = 'get_group_list_failure';
@@ -13,7 +14,7 @@ const INITIAL_STATE = {
 const getGroupListSuccess = groupList => {
   return {
     type: GET_GROUP_LIST_SUCCESS,
-    payload: groupList,
+    payload: {groupList: groupList},
   };
 };
 
@@ -24,24 +25,24 @@ const getGroupListFailure = error => {
   };
 };
 
-export const getGroupList = () => {
-  return dispatch => {
-    const options = {
-      url: `${Config.REACT_APP_SERVER_ENDPOINT}api/v1/groups/`,
-      method: 'GET',
-    };
-    axios(options)
-      .then(res => {
-        if (res.status === responses.SUCCESS) {
-          dispatch(getGroupListSuccess(res.data.members));
-        } else {
-          throw new Error(res.err);
-        }
-      })
-      .catch(err => {
-        dispatch(getGroupListFailure(err.message));
-      });
+export const getGroupList = () => async dispatch =>{
+  const token = await AsyncStorage.getItem('token');
+
+  const options = {
+    url: `${Config.REACT_APP_SERVER_ENDPOINT}api/v1/groups/`,
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
   };
+
+  try {
+    const response = await axios(options);
+    dispatch(getGroupListSuccess(response.data.groups));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -49,7 +50,7 @@ export default (state = INITIAL_STATE, action) => {
     case GET_GROUP_LIST_SUCCESS:
       return {
         ...state,
-        groups: action.payload,
+        ...action.payload,
       };
     case GET_GROUP_LIST_FAILURE:
       return {
