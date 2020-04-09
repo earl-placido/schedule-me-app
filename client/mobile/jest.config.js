1a242fcc2d303ee45a18124c4098a182e7eb8061
Bonnie Tang <35672951+bonniet@users.noreply.github.com>
2020-04-09 17:29:17 -0500

    Fix login tests

--- client/mobile/jest.config.js
+++ client/mobile/jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   setupFiles: [
     '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
     '<rootDir>/jest/setup.js',
+    '<rootDir>/__mocks__/@react-native-community/google-signin.js',
   ],
   testMatch: ['<rootDir>/__tests__/**/*.test.js'],
   transformIgnorePatterns: ['/node_modules/(?!native-base)/'],
